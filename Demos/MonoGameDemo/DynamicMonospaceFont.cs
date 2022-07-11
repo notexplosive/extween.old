d612f380103c41d3d243f6cc5255c3640a893877
notexplosive <thenotexplosive@gmail.com>
2022-07-10 18:29:04 -0700

    Extract methods until readable

--- Demos/MonoGameDemo/DynamicMonospaceFont.cs
+++ Demos/MonoGameDemo/DynamicMonospaceFont.cs
@@ -18,7 +18,7 @@ namespace MonoGameDemo
             {
                 return new TweenPattern(new SequenceTween(), x, y);
             }
-            
+
             CallbackTween SetXY(float targetX, float targetY)
             {
                 return new CallbackTween(
@@ -29,115 +29,63 @@ namespace MonoGameDemo
                     });
             }
 
+            Tween<float> AxisLine(Tweenable<float> tweenable, float destination)
+            {
+                return new Tween<float>(tweenable, destination, duration, Ease.Linear);
+            }
+
+            MultiplexTween ArcStart(float targetX, float targetY)
+            {
+                return new MultiplexTween()
+                    .AddChannel(new Tween<float>(x, targetX, duration, Ease.SineSlowFast))
+                    .AddChannel(new Tween<float>(y, targetY, duration, Ease.SineFastSlow));
+            }
+
+            MultiplexTween ArcEnd(float targetX, float targetY)
+            {
+                return new MultiplexTween()
+                    .AddChannel(new Tween<float>(x, targetX, duration, Ease.SineFastSlow))
+                    .AddChannel(new Tween<float>(y, targetY, duration, Ease.SineSlowFast));
+            }
+
             switch (letter)
             {
                 case 'H':
                     tween = new SequenceTween()
-                            .Add(
-                                SetXY(-0.5f, -1)
-                            )
-                            .Add(
-                                new Tween<float>(y, 1, duration, Ease.Linear)
-                            )
-                            .Add(
-                                new Tween<float>(y, 0, duration, Ease.Linear)
-                            )
-                            .Add(
-                                new Tween<float>(x, 0.5f, duration, Ease.Linear)
-                            )
-                            .Add(
-                                new Tween<float>(y, -1, duration, Ease.Linear)
-                            )
-                            .Add(
-                                new Tween<float>(y, 1, duration, Ease.Linear)
-                            )
+                            .Add(SetXY(-0.5f, -1))
+                            .Add(AxisLine(y, 1))
+                            .Add(AxisLine(y, 0))
+                            .Add(AxisLine(x, 0.5f))
+                            .Add(AxisLine(y, -1f))
+                            .Add(AxisLine(y, 1f))
                         ;
                     break;
 
                 case 'e':
                     tween = new SequenceTween()
-                            .Add(
-                                new CallbackTween(
-                                    () =>
-                                    {
-                                        x.ForceSetValue(-0.5f);
-                                        y.ForceSetValue(0.5f);
-                                    })
-                            )
-                            .Add(
-                                new Tween<float>(x, 0.5f, duration, Ease.Linear)
-                            )
-                            .Add(
-                                new MultiplexTween()
-                                    .AddChannel(new Tween<float>(x, 0f, duration, Ease.SineSlowFast))
-                                    .AddChannel(new Tween<float>(y, 0f, duration, Ease.SineFastSlow))
-                            )
-                            .Add(
-                                new MultiplexTween()
-                                    .AddChannel(new Tween<float>(x, -0.5f, duration, Ease.SineFastSlow))
-                                    .AddChannel(new Tween<float>(y, 0.5f, duration, Ease.SineSlowFast))
-                            )
-                            .Add(
-                                new MultiplexTween()
-                                    .AddChannel(new Tween<float>(x, 0f, duration, Ease.SineSlowFast))
-                                    .AddChannel(new Tween<float>(y, 1f, duration, Ease.SineFastSlow))
-                            )
-                            .Add(
-                                new MultiplexTween()
-                                    .AddChannel(new Tween<float>(x, MathF.Cos(MathF.PI / 4) * 0.5f, duration,
-                                        Ease.SineFastSlow))
-                                    .AddChannel(new Tween<float>(y, MathF.Sin(MathF.PI / 4) * 0.5f + 0.5f,
-                                        duration, Ease.SineSlowFast))
-                            )
+                            .Add(SetXY(-0.5f, 0.5f))
+                            .Add(AxisLine(x, 0.5f))
+                            .Add(ArcStart(0, 0))
+                            .Add(ArcEnd(-0.5f, 0.5f))
+                            .Add(ArcStart(0, 1))
+                            .Add(ArcEnd(MathF.Cos(MathF.PI / 4) * 0.5f, MathF.Sin(MathF.PI / 4) * 0.5f + 0.5f))
                         ;
                     break;
 
                 case 'l':
                     tween = new SequenceTween()
-                            .Add(
-                                new CallbackTween(
-                                    () =>
-                                    {
-                                        x.ForceSetValue(0f);
-                                        y.ForceSetValue(-1f);
-                                    })
-                            )
-                            .Add(
-                                new Tween<float>(y, 1f, duration, Ease.Linear)
-                            )
+                            .Add(SetXY(0, -1))
+                            .Add(AxisLine(y, 1))
                         ;
                     break;
 
                 case 'o':
                     tween = new SequenceTween()
-                            .Add(
-                                new CallbackTween(
-                                    () =>
-                                    {
-                                        x.ForceSetValue(0.5f);
-                                        y.ForceSetValue(0.5f);
-                                    })
-                            )
-                            .Add(
-                                new MultiplexTween()
-                                    .AddChannel(new Tween<float>(x, 0, duration, Ease.SineSlowFast))
-                                    .AddChannel(new Tween<float>(y, 1f, duration, Ease.SineFastSlow))
-                            )
-                            .Add(
-                                new MultiplexTween()
-                                    .AddChannel(new Tween<float>(x, -0.5f, duration, Ease.SineFastSlow))
-                                    .AddChannel(new Tween<float>(y, 0.5f, duration, Ease.SineSlowFast))
-                            )
-                            .Add(
-                                new MultiplexTween()
-                                    .AddChannel(new Tween<float>(x, 0, duration, Ease.SineSlowFast))
-                                    .AddChannel(new Tween<float>(y, 0f, duration, Ease.SineFastSlow))
-                            )
-                            .Add(
-                                new MultiplexTween()
-                                    .AddChannel(new Tween<float>(x, 0.5f, duration, Ease.SineFastSlow))
-                                    .AddChannel(new Tween<float>(y, 0.5f, duration, Ease.SineSlowFast))
-                            )
+                            .Add(SetXY(0.5f, 0.5f))
+                            .Add(ArcStart(0, 1))
+                            .Add(ArcEnd(-0.5f, 0.5f))
+                            .Add(ArcStart(0f, 0f))
+                            .Add(ArcEnd(0.5f, 0.5f))
                         ;
                     break;
             }
@@ -146,34 +94,11 @@ namespace MonoGameDemo
             {
                 // Default letter is just a circle
                 tween = new SequenceTween()
-                        .Add(
-                            new CallbackTween(
-                                () =>
-                                {
-                                    x.ForceSetValue(1);
-                                    y.ForceSetValue(0);
-                                })
-                        )
-                        .Add(
-                            new MultiplexTween()
-                                .AddChannel(new Tween<float>(x, 0, duration, Ease.SineSlowFast))
-                                .AddChannel(new Tween<float>(y, 1, duration / 4, Ease.SineFastSlow))
-                        )
-                        .Add(
-                            new MultiplexTween()
-                                .AddChannel(new Tween<float>(x, -1, duration, Ease.SineFastSlow))
-                                .AddChannel(new Tween<float>(y, 0, duration, Ease.SineSlowFast))
-                        )
-                        .Add(
-                            new MultiplexTween()
-                                .AddChannel(new Tween<float>(x, 0, duration, Ease.SineSlowFast))
-                                .AddChannel(new Tween<float>(y, -1, duration, Ease.SineFastSlow))
-                        )
-                        .Add(
-                            new MultiplexTween()
-                                .AddChannel(new Tween<float>(x, 1, duration, Ease.SineFastSlow))
-                                .AddChannel(new Tween<float>(y, 0, duration, Ease.SineSlowFast))
-                        )
+                        .Add(SetXY(1, 0))
+                        .Add(ArcStart(0, 1))
+                        .Add(ArcEnd(-1, 0))
+                        .Add(ArcStart(0, -1))
+                        .Add(ArcEnd(1, 0))
                     ;
             }
 
