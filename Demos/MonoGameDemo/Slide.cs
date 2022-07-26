8298c7ea1464101824d7c62deedaccd44c56d528
notexplosive <thenotexplosive@gmail.com>
2022-07-25 21:38:04 -0700

    Extract file

---
+++ Demos/MonoGameDemo/Slide.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using ExTween;
+using ExTween.Art;
+
+namespace MonoGameDemo
+{
+    public abstract class Slide : ICue
+    {
+        private readonly SequenceTween tween = new SequenceTween();
+        protected List<ISlideElement> Elements { get; } = new List<ISlideElement>();
+        protected List<Action> UpdateFunctions { get; } = new List<Action>();
+
+        public void Setup()
+        {
+            this.tween.Clear();
+            Elements.Clear();
+            UpdateFunctions.Clear();
+            
+            BuildTween(this.tween);
+        }
+
+        protected abstract void BuildTween(SequenceTween sequenceTween);
+        
+        public void Draw(Painter painter)
+        {
+            foreach (var updateFunction in UpdateFunctions)
+            {
+                updateFunction();
+            }
+            
+            foreach (var glyph in Elements)
+            {
+                glyph.Draw(painter);
+            }
+        }
+
+        public bool IsDone()
+        {
+            return this.tween.IsDone();
+        }
+
+        public void UpdateTween(float dt)
+        {
+            this.tween.Update(dt);
+        }
+    }
+}
--- Demos/MonoGameDemo/SlideDeck.cs
+++ Demos/MonoGameDemo/SlideDeck.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using ExTween;
+﻿using System.Collections.Generic;
 using ExTween.Art;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
@@ -97,46 +95,5 @@ namespace MonoGameDemo
     public interface ICue
     {
         public bool IsDone();
-    }
-
-    public abstract class Slide : ICue
-    {
-        private readonly SequenceTween tween = new SequenceTween();
-        protected List<ISlideElement> Elements { get; } = new List<ISlideElement>();
-        protected List<Action> UpdateFunctions { get; } = new List<Action>();
-
-        public void Setup()
-        {
-            this.tween.Clear();
-            Elements.Clear();
-            UpdateFunctions.Clear();
-            
-            BuildTween(this.tween);
-        }
-
-        protected abstract void BuildTween(SequenceTween sequenceTween);
-        
-        public void Draw(Painter painter)
-        {
-            foreach (var updateFunction in UpdateFunctions)
-            {
-                updateFunction();
-            }
-            
-            foreach (var glyph in Elements)
-            {
-                glyph.Draw(painter);
-            }
-        }
-
-        public bool IsDone()
-        {
-            return this.tween.IsDone();
-        }
-
-        public void UpdateTween(float dt)
-        {
-            this.tween.Update(dt);
-        }
     }
 }
